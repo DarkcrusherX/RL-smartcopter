343c6d5f6349503ca10e642f675e5ba61306de8a
DarkcrusherX <ahalmartinv@gmail.com>
2020-08-05 20:29:00 +0530

    some error in px4_connection

--- src/px4_connection.py
+++ src/px4_connection.py
@@ -8,15 +8,16 @@ import signal
 
 class Px4Connection():
     
-    def __init__(self):
+    # def __init__(self):
         
     def launch(self):
         process = subprocess.Popen('roslaunch RL-smartcopter px4.launch', stdout=subprocess.PIPE, shell=True)
         for  i in range(10):
             print("Wait for  px4.launch to connect (10 sec): {}".format(i))
             time.sleep(1)
+        return process
 
-    def kill(self):
+    def kill(self,process):
         os.kill(process.pid, signal.SIGKILL)
         for  i in range(10):
             print("Wait for  px4.launch to die (10 sec): {}".format(i))
--- src/trial_px4_connection.py
+++ src/trial_px4_connection.py
@@ -6,29 +6,45 @@ import time
 import rospy
 import signal
 from armf import armtakeoff
+from px4_connection import Px4Connection
 
 lul = armtakeoff()
+lol = Px4Connection()
 
 rospy.init_node('bruh_onli', anonymous=True)
 
 again = 'y'
 
+# while again=='y':
+
+#     process = subprocess.Popen('roslaunch RL-smartcopter px4.launch', stdout=subprocess.PIPE, shell=True)
+#     print(process)
+#     print(type(process))
+#     for  i in range(5):
+#         print(i)
+#         time.sleep(1)
+#     lul.arm()
+
+#     i = input("Do u want to kill this process")
+
+#     os.kill(process.pid, signal.SIGKILL)
+
+#     for  i in range(5):
+#         print(i)
+#         time.sleep(1)
+    
+#     again = input("Do u want to go again: ")
+
 while again=='y':
 
-    process = subprocess.Popen('roslaunch RL-smartcopter px4.launch', stdout=subprocess.PIPE, shell=True)
-    print(process)
-    print(type(process))
-    for  i in range(5):
-        print(i)
-        time.sleep(1)
+    process = lol.launch()
+
     lul.arm()
+    lul.takeoff()
 
-    i = input("Do u want to kill this process")
+    lol.kill(process)
 
-    os.kill(process.pid, signal.SIGKILL)
-
-    for  i in range(5):
-        print(i)
-        time.sleep(1)
-    
     again = input("Do u want to go again: ")
+
+
+
