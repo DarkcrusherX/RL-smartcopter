c65943be09401699f5b2a51d6e7e5add03828ae3
DarkcrusherX <ahalmartinv@gmail.com>
2020-08-17 19:45:55 +0530

    px4 connection not fixed

--- src/px4_connection.py
+++ src/px4_connection.py
@@ -18,7 +18,15 @@ class Px4Connection():
         return process
 
     def kill(self,process):
-        os.kill(process.pid, signal.SIGKILL)
+        # os.kill(process.pid, signal.SIGKILL)
+        child_pid = process.pid
+        print("Process pid to be death awaits u all is : {}".format(process.pid))
+        if child_pid is None:
+            pass
+        else:
+            # os.kill(child_pid, signal.SIGINT)
+            # process.terminate()
+            process.send_signal(signal.SIGINT)
         for  i in range(10):
             print("Wait for  px4.launch to die (10 sec): {}".format(i))
             time.sleep(1)
