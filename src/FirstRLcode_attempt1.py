52157e53505890884cd04564d044b870f33b6e9b
DarkcrusherX <ahalmartinv@gmail.com>
2020-10-06 17:21:19 +0530

    yeyeyye

--- src/FirstRLcode_attempt1.py
+++ src/FirstRLcode_attempt1.py
@@ -7,10 +7,11 @@ import math
 from collections import deque
 from geometry_msgs.msg import TwistStamped
 from geometry_msgs.msg import PoseStamped
+import tf
+from sensor_msgs.msg import Imu
 
 dbridge = CvBridge()
 
-import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Input, Conv2DTranspose, Concatenate, Flatten, Dense , Activation , Dropout , BatchNormalization
@@ -18,6 +19,7 @@ from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 from tensorflow.keras import backend as K
 from tensorflow.keras.utils import plot_model
+from std_msgs.msg import String
 
 dbridge = CvBridge()
 rospy.init_node('RL_agent', anonymous=True)
@@ -30,28 +32,52 @@ class Agent:
         self.nruns = 1000
         self.sub_image = rospy.Subscriber("/camera/depth/image_raw", Image, self.image_callback)
         self.rospy.Subscriber('mavros/local_position/pose',PoseStamped,self.current_pos_callback)
+        self.imu = rospy.Subscriber("/mavros/imu/data", Imu , self.imu_callback)
+        self.goal_sub = rospy.Subscriber("goal", PoseStamped , self.goal_callback)
         self.velocity_publisher = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',Twist, queue_size=10)
         self.drone_qvalues_model = self.drone_model()
         self.target_drone_qvalues_model = self.drone_model()
         self.memory = deque(maxlen=50)
         self.target_count = 0
         self.current_pos = PoseStamped()
+        self.imu_data = None
         self.rate = rospy.Rate(10)
         self.collision = False
         self.target_reached = False
-        self.goal_radius = #////////set this
+        self.goal_radius = 0.5
         self.rewards = []
         self.episode = []
+        self.max_steps = 0
+        self.max_incl = 0.4
+        self.goal = PoseStamped()
+        self.imu_check = rospy.Subscriber("pitch_roll_fail", String, self.imu_callback)
 
-    def image_callback(self):
+    def image_callback(self,img_msg):
         self.current_state = dbridge.imgmsg_to_cv2(img_msg,"8UC1")
         cv2.imshow(self.current_state)
 
     def current_pos_callback(self,position):
         self.current_pos = position
+    
+    def imu_callback(self, imu_data_1):
+        self.imu_data = imu_data_1.data
 
-    def check_if_done(self, goal, current_pos):
-        if '''collision has happened''': #/////////////////////
+    def goal_callback(self, goal_pos):
+        self.goal = goal_pos
+
+    def check_if_done(self, goal):
+        # euler = tf.transformations.euler_from_quaternion([self.imu_data.orientation.x,self.imu_data.orientation.y,self.imu_data.orientation.z,self.imu_data.orientation.w])
+        # roll = euler[0]
+        # pitch = euler[1]
+        # yaw = euler[2]
+        # roll_bad = not(-self.max_incl < roll < self.max_incl)
+        # pitch_bad = not(-self.max_incl < pitch < self.max_incl)
+        # if pitch_bad==True or roll_bad==True:
+        #     self.collision = True
+        #     return True
+        current_pos = self.current_pos
+        current_pos = np.array([current_pos.pose.position.x, current_pos.pose.position.y, current_pos.pose.position.z])
+        if self.imu_data == "Fail":
             self.collision = True
             return True
         goal_distance = math.sqrt((math.pow(goal[0]-current_pos[0],2)) + (math.pow(goal[1]-current_pos[1],2)) + (math.pow(goal[2]-current_pos[2],2)))
@@ -158,7 +184,7 @@ class Agent:
             total_reward = 0
             #start gazebo,px4,spawn and setup(arming and stuff) the  drone
             #set the goal_pos coordinates in an array/////////////////
-            while('''episode not over'''): #/////////////////
+            while(self.collision == True or self.target_reached == True): #/////////////////
                 for i in range(50):
                     current_state = self.current_state
                     current_pos = self.current_pos
@@ -172,31 +198,38 @@ class Agent:
                 current_pos = self.current_pos
                 current_pos = np.array([current_pos.pose.position.x, current_pos.pose.position.y, current_pos.pose.position.z])
                 #this is the relative current position
-                current_pos[0] = goal_pos[0] - current_pos[0]
-                current_pos[1] = goal_pos[1] - current_pos[1]
-                current_pos[2] = goal_pos[2] - current_pos[2]
+                goal_pos = np.array([self.goal.pose.position.x, self.goal.pose.position.y, self.goal.pose.position.z])
+                current_pos_rel[0] = goal_pos[0] - current_pos[0]
+                current_pos_rel[1] = goal_pos[1] - current_pos[1]
+                current_pos_rel[2] = goal_pos[2] - current_pos[2]
                 action = take_action(self,current_state,current_pos)
                 next_state = self.current_state
                 next_pos = self.current_pos
-                done = self.check_if_done(current_state,current_pos,next_state,next_pos)
+                # done = self.check_if_done(current_state,current_pos,next_state,next_pos)
+                done = self.check_if_done(goal_pos)
                 reward = self.reward()
                 total_reward += reward
                 self.memory.append([current_state,current_pos, action, reward, next_state, next_pos, done])
                 self.train_network()
                 self.epsilon = max(0.01,self.epsilon*0.995)
+                self.max_steps += 1
                 if self.target_count == 1000:
                     self.update_target_network
                     self.target_count == 0
-                if self.collison == True or self.target_reached == True:
+                if self.collison == True or self.target_reached == True or self.max_steps==20000:
                     self.collison = False
                     self.target_reached = False
+                    self.max_steps = 0
                     episode_number += 1
                     break
             self.rewards.append(total_reward)
             self.episode.append(episode_number)
+            print("Reward for episode ",episode_number,":",total_reward)
     return self.rewards, self.episode, self.drone_qvalues_model
             #////////kill px4, gazebo
 
 import matplotlib.pyplot as plt
 rewards, episode_list, qvalues_model = Agent.execute()
 plt.plot(episode_list,rewards)
+        
+
